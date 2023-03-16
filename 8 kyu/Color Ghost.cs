466797c84a12ffd26cab913f53616ea7181a2465
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-16 23:50:59 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Color Ghost.cs
@@ -0,0 +1,21 @@
+// -----------------------------------------------------------
+// Color Ghost
+// Create a class Ghost
+// 
+// Ghost objects are instantiated without any arguments.
+// 
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+// 
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+// -----------------------------------------------------------
+
+using System;
+
+public class Ghost{
+  string[] colorGhost = {"purple", "red", "white", "yellow"};
+  
+  public string GetColor(){
+    return colorGhost[new Random().Next(0, colorGhost.Length)];
+  }
+}
