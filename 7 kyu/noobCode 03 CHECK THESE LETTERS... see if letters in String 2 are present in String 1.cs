0c61d8278dbf55521cd2c1bbd31e08b050cc2143
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-20 22:48:25 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/noobCode 03 CHECK THESE LETTERS... see if letters in String 2 are present in String 1.cs
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------
+// Write a function that checks if all the letters in the second string are present in the first one at least once, regardless of how many times they appear:
+// 
+// ["ab", "aaa"]    =>  true
+// ["trances", "nectar"]    =>  true
+// ["compadres", "DRAPES"]  =>  true
+// ["parses", "parsecs"]    =>  false
+// 
+// Function should not be case sensitive, as indicated in example #2. Note: both strings are presented as a single argument in the form of an array.
+// -----------------------------------------------------------
+
+using System.Linq;
+
+public class Kata
+{
+	public static bool LetterCheck(string[] arr)
+	{
+		string a = arr[0].ToLower();
+		string b = arr[1].ToLower();
+		return b.All(letter => a.Contains(letter));
+	}
+}
