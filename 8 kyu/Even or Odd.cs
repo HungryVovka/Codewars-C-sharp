97f4f5b014ea45d25ef08a62251d28a5917d57f9
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-20 22:36:40 +0500

    feat: 8 kyu solutions

--- 8 kyu/Even or Odd.cs
+++ 8 kyu/Even or Odd.cs
@@ -4,10 +4,11 @@
 
 using System;
 
-namespace Solution{
-  public class SolutionClass{
-    public static string EvenOrOdd(int number){
-      return number % 2 == 0 ? "Even" : "Odd";
+namespace Solution
+{
+    public class SolutionClass{
+        public static string EvenOrOdd(int number){
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
     }
-  }
 }
