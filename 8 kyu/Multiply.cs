5a0f893b790dec168f823269c50fc59f5d36ef2d
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-20 22:39:57 +0500

    feat: 8 kyu solutions

--- 8 kyu/Multiply.cs
+++ 8 kyu/Multiply.cs
@@ -2,8 +2,10 @@
 // This code does not execute properly. Try to figure out why.
 // -----------------------------------------------------------
 
-public class CustomMath {
-    public static int multiply(int a, int b){
+public class CustomMath
+{
+    public static int multiply(int a, int b)
+    {
         return a * b;
     }
 }
