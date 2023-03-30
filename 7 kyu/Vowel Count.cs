ad9581b4ce0166b12b29992cc20987dbdb7e159a
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-30 23:54:48 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Vowel Count.cs
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------
+// Return the number (count) of vowels in the given string.
+// 
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// 
+// The input string will only consist of lower case letters and/or spaces.
+// -----------------------------------------------------------
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+    	int vowelCount = str.Count(i => "aeiouAEIOU".Contains((char)i));
+        return vowelCount;
+    }
+}
