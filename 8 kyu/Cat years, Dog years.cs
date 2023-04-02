46ce22ec1421b2bb4b68bdd7855e5842b749e4b7
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-02 23:31:37 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Cat years, Dog years.cs
@@ -0,0 +1,54 @@
+// -----------------------------------------------------------
+// Kata Task
+// I have a cat and a dog.
+// 
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+// 
+// Return their respective ages now as [humanYears,catYears,dogYears]
+// 
+// NOTES:
+// 
+// humanYears >= 1
+// humanYears are whole numbers only
+// 
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// 
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+// 
+// References
+// 
+// http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+// http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+// -----------------------------------------------------------
+
+public class Dinglemouse
+{
+	public static int[] humanYearsCatYearsDogYears(int humanYears)
+	{
+		int catYears1 = 15;
+		int catYears2 = catYears1 + 9;
+		int catYears3 = catYears2 + (humanYears - 2) * 4;
+		int dogYears1 = 15;
+		int dogYears2 = dogYears1 + 9;
+		int dogYears3 = dogYears2 + (humanYears - 2) * 5;
+		if (humanYears == 1)
+		{
+			return new int[]{humanYears, catYears1, dogYears1};
+		}
+		if (humanYears == 2)
+		{
+			return new int[]{humanYears, catYears2, dogYears2};
+		}
+		if (humanYears > 2)
+		{
+			return new int[]{humanYears, catYears3, dogYears3};
+		}
+		return new int[]{0, 0, 0};
+	}
+}
---
+++ 8 kyu/Function 2 - squaring an argument.cs
@@ -0,0 +1,11 @@
+// -----------------------------------------------------------
+// Now you have to write a function that takes an argument and returns the square of it.
+// -----------------------------------------------------------
+
+public class Kata
+{
+	public static double Square(double n)
+	{
+		return n * n;
+	}
+}
