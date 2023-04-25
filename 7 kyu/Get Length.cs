3526f5c5f58ecf5bcdccb2c91e5d055f138e868a
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-26 01:26:16 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Get Length.cs
@@ -0,0 +1,36 @@
+// -----------------------------------------------------------
+// Get Length
+// You don't have any idea what is the type of the input object, but you know that it has Length property. (The input can be any type of object that has 
+// Length property)
+// 
+// So, the GetLength method should return the Length of input.
+// -----------------------------------------------------------
+
+public class Kata
+{
+	public static int GetLength(dynamic inputObject)
+	{
+		return inputObject.Length;
+	}
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-C-sharp
+// (https://github.com/HungryVovka/Codewars-C-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-C-sharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
