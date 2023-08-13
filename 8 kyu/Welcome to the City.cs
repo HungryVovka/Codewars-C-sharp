82687e57337647b7199dd163ec85aa9bae7102ef
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-08-14 04:07:44 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Welcome to the City.cs
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values 
+// that should be joined together with one space between each, and the length of the name array in test cases will vary.
+// 
+// Example:
+// 
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+// -----------------------------------------------------------
+
+public class Kata
+{
+	public static string SayHello(string[] name, string city, string state)
+	{
+		return $"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!";
+	}
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-C-sharp
+// (https://github.com/HungryVovka/Codewars-C-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-C-sharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
