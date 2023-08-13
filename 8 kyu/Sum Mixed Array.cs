867c2cf15ccdc9a13fa6649b5a2e6c7b06fa717a
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-08-14 00:39:18 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Sum Mixed Array.cs
@@ -0,0 +1,37 @@
+// -----------------------------------------------------------
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// 
+// Return your answer as a number.
+// -----------------------------------------------------------
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int SumMix(object[] x)
+  {
+    return x.Sum(Convert.ToInt32);
+  }
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-C-sharp
+// (https://github.com/HungryVovka/Codewars-C-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-C-sharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
